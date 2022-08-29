02d1a8cda79419f42ad17963cc72965a9322d067
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-29 01:26:17 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):

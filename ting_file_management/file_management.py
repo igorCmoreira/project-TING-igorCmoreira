22828cf335f8abe5b2f469992377786427b5269b
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-29 00:30:58 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação """
+    try:
+        if not path_file.endswith(".txt"):
+            return print("Formato inválido", file=sys.stderr)
+        with open(path_file, encoding="utf-8") as file:
+            return [line.strip() for line in file.readlines()]
+
+    except FileNotFoundError:
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

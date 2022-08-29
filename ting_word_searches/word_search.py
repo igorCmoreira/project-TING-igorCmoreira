dd72e3ddb0c9dd37d4b9caa70de53ae0c1252c69
Igor Campos Moreira <igorcampos200@gmail.com>
2022-08-29 02:20:15 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -16,4 +16,17 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrence = []
+    words_report = []
+    for index, line in enumerate(instance._data[0]['linhas_do_arquivo']):
+        if word.lower() in line.lower():
+            occurrence.append({'linha': index + 1, "conteudo": line})
+    if len(occurrence) > 0:
+        words_report = [
+            {
+                "palavra": word,
+                "arquivo": instance._data[0]['nome_do_arquivo'],
+                "ocorrencias": occurrence,
+            }
+        ]
+    return words_report
